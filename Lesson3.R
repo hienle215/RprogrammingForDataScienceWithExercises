c414b7061ede29bb225d7a871c26db0997da2b94
Thi Le (TAU) <hien.le@tuni.fi>
2021-05-29 00:17:35 +0300

    Logical variables and operators

---
+++ Lesson3.R
@@ -0,0 +1,28 @@
+# Logical
+
+# TRUE
+
+# FALSE
+ 4 < 5
+10 > 100 
+ 4== 5
+
+ # ==
+ # !=
+ # <
+ # >
+ # >=
+ # <=
+ # ! not
+ # & anh
+ # isTRUE() checking true logical
+ 
+ result <- 4<5
+result
+typeof(result)
+
+result2 <- !TRUE
+result2
+
+result | result2
+result & result2
