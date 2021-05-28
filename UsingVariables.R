ea059a613ce103e2b0657e3a9eef5c77b08ccb2c
Thi Le (TAU) <hien.le@tuni.fi>
2021-05-29 00:09:32 +0300

    Using variables in R

---
+++ UsingVariables.R
@@ -0,0 +1,17 @@
+A <- 10
+B <- 5
+C <- A + B
+C
+var <- 2.5
+var2 <- 4
+
+result <- var/var2
+result
+
+answer <- sqrt(var2)
+answer
+
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
