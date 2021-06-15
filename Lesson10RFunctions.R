6f160b48224b81292561e66a78117c4806f5474d
Thi Le (TAU) <hien.le@tuni.fi>
2021-06-15 10:36:38 +0300

    Basic functions in R

---
+++ Lesson10RFunctions.R
@@ -0,0 +1,22 @@
+rnorm()
+
+c()
+seq()
+rep()
+
+print()
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typeof()
+sqrt()
+paste()
+
+#?
+?rnorm()
+
+seq(from=10, to=20, length.out=100)
+seq(from=10, to=20,along.with=x)
+rep(5,10)
