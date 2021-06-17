e0fc0cc89f15821dde37520b98aac140712fa0bc
Thi Le (TAU) <hien.le@tuni.fi>
2021-06-17 15:17:32 +0300

    Explain the graph from matrix

---
+++ Lesson18BasketBallInsight.R
@@ -0,0 +1,22 @@
+
+#Salary
+myplot(Salary)
+myplot(Salary/Games)
+myplot(Salary/FieldGoals)
+
+# In-Game metrics
+myplot(MinutesPlayed)
+myplot(Points)
+
+# In-game metrics normalized
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
+
+#Intersting observation
+myplot(MinutesPlayed/Games)
+myplot(Games)
+
+#Times is valuable
+myplot(Points/FieldGoals)
